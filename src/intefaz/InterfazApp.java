3405b7db4ce4358e9fab5ef2b1696550c5fe8eb1
FelipeGo18 <gonzalezgarzon14@hotmail.com>
2024-08-23 11:46:55 -0500

    2 jaja

--- src/intefaz/InterfazApp.java
+++ src/intefaz/InterfazApp.java
@@ -4,19 +4,35 @@
  */
 package intefaz;
 
+import java.awt.Color;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
 /**
  *
  * @author gonza
  */
-public class InterfazApp {
-    
-    
-    
-    
-    
-    
-    public static void main(String[] args) {
+public class InterfazApp extends JFrame {
+
+    public InterfazApp() {
+        //Propiedades de la interfaz o frame
+        setTitle("Nonograma");
+        getContentPane().setLayout(null);  //Tipo de layout, en este caso null
+        setSize(480, 325);  //Tamaño del Frame
+        setLocationRelativeTo(null);  //Centra el Frame
+        setResizable(false);  //No permite cambiar el tamaño del frame
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Cerrar programa cuando se le da a la X
+        
+        //Imagen a la barra de tareas
+        ImageIcon icon = new ImageIcon(getClass().getResource("/resources/icono6.png"));
+        Image image = icon.getImage();
+        setIconImage(image);
         
     }
-    
+
+    public static void main(String[] args) {
+
+    }
+
 }
