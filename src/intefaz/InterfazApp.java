3b05e4fceab6035fb9e68e4e8bce69998cd9b929
FelipeGo18 <gonzalezgarzon14@hotmail.com>
2024-08-23 11:33:13 -0500

    1. poier el main

--- src/intefaz/InterfazApp.java
+++ src/intefaz/InterfazApp.java
@@ -1,14 +1,22 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package intefaz;
 
 /**
  *
- * @author asus
+ * @author gonza
  */
 public class InterfazApp {
     
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        
+    }
     
 }
